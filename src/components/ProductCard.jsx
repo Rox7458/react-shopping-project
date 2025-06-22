9fd00417b28fb912a9de0fa5093a9223fd8bb53a
Rox7458 <jagaaoguz.7000@gmail.com>
2025-06-22 23:19:26 +0300

    f

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,12 @@
+import { useSelector } from "react-redux";
+
 const ProductCard = ({ product }) => {
+  const { state } = useSelector;
+
+  const handleAdd = () => {
+    
+  };
+
   return (
     <div className="m-2">
       <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700">
@@ -17,8 +25,9 @@ const ProductCard = ({ product }) => {
           <a
             href="#"
             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            onClick={() => handleAdd()}
           >
-            Read more
+            Add basket
             <svg
               class="rtl:rotate-180 w-3.5 h-3.5 ms-2"
               aria-hidden="true"
--- src/redux/reducer/basketReducer.jsx
+++ src/redux/reducer/basketReducer.jsx
@@ -1,13 +1,18 @@
-const basketReducer = (state="", { type, payload }) => {
+const basketReducer = (state = [], { type, payload }) => {
+  console.log(state);
+
   switch (type) {
     case "DELETE":
-      return;
+      return { yapilacaklar: state.yapilacaklar.filter((a) => a !== payload) };
 
     case "ADD":
-      return;
+      return {
+        yapilacaklar: [...state.yapilacaklar, { id: 3, task: payload, completed: false }],
+      };
 
     case "":
       return;
+    // dizide dolaş, müşterinin gönderdiği id ile eşleşen elemanın, diğer özellikleri kalsın, completed i değiştir
 
     default:
       return state;
